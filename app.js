96cdcaed9002ad9c231169ae4d4342a32bf7bd61
Denis Manuel Lim <denismanuellim@Deniss-MacBook-Pro.local>
2018-01-07 16:22:54 -0800

    modify page with bootstrap

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ var app = express();
 
 app.set("view engine", "ejs");
 
+app.use(express.static(__dirname + '/public'));
+
 
 app.get("/", function(req, res) {
    res.render("landing")
 });
 
-app.use(express.static(path.join(__dirname, '/public')));
 
 app.listen(process.env.PORT, process.env.IP, function() {
    console.log("Website server has started...");
